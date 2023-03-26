e90910fb7fb619f68898212edd2bec816208e6f2
jelipo <me@jelipo.com>
2023-03-26 16:44:53 +0800

    update

--- hashmap/src/hashmap.bpf.c
+++ hashmap/src/hashmap.bpf.c
@@ -1,7 +1,5 @@
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
-#include <bpf/bpf_core_read.h>
-#include <bpf/bpf_tracing.h>
 
 char __license[] SEC("license") = "Dual MIT/GPL";
 
@@ -9,13 +7,14 @@ struct {
     __uint(type, BPF_MAP_TYPE_HASH);
     __uint(max_entries, 1024);
     __type(key, u32);
-    __type(value, int32);
+    __type(value, u32);
 } my_map SEC(".maps");
 
 SEC("tracepoint/syscalls/sys_enter_execve")
 int bpf_prog(void *ctx) {
-    char key[64] = "key";
+    char key[4] = "key";
     int *value = NULL;
+    bpf_printk("%c%c%c", key[0], key[1], key[2]);
     value = bpf_map_lookup_elem(&my_map, &key);
     if (value != NULL) {
         *value += 1;
--- hashmap/src/main.rs
+++ hashmap/src/main.rs
@@ -1,11 +1,32 @@
-use std::collections::HashMap;
+use std::thread;
+use std::time::Duration;
+
+use anyhow::Result;
+use libbpf_rs::MapFlags;
+
 use crate::runqslower::HashmapSkelBuilder;
 
 mod runqslower {
     include!("./hashmap.bpf.rs");
 }
 
-fn main() {
-    HashmapSkelBuilder::open()
-    println!("Hello, world!");
+fn main() -> Result<()> {
+    let result = common::bump_memlock_rlimit();
+    let builder = HashmapSkelBuilder::default();
+    let open_skel = builder.open()?;
+    // TODO 写入参数
+    let mut skel = open_skel.load()?;
+    skel.attach()?;
+    let mut key_bytes = [0u8; 4];
+    key_bytes[..3].copy_from_slice("key".as_bytes());
+    skel.maps_mut().my_map().update(&key_bytes, &key_bytes, MapFlags::ANY)?;
+    loop {
+        thread::sleep(Duration::from_secs(3));
+        let option = skel.maps_mut().my_map().lookup(&key_bytes, MapFlags::ANY)?;
+        match option {
+            None => { println!("无数据") }
+            Some(data) => { println!("数据长度：{}", data.len()); }
+        }
+    }
+    Ok(())
 }
