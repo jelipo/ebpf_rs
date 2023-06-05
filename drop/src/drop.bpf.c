e9955f335476da6dce6f305804b532ec9e62c832
jelipo <me@jelipo.com>
2023-06-05 10:47:08 +0800

    update

--- drop/src/drop.bpf.c
+++ drop/src/drop.bpf.c
@@ -5,8 +5,18 @@
 #include <bpf/bpf_tracing.h>
 #include <bpf/bpf_endian.h>
 #include <linux/pkt_cls.h>
+#include <bpf/bpf_core_read.h>
 
-char __license[] SEC("license") = "Dual MIT/GPL";;
+char __license[] SEC("license") = "Dual MIT/GPL";
+
+struct key_t {
+    char h_dest_mac[16];
+    char h_src_mac[16];
+    u16 h_proto;
+    u64 user_stack_id;
+    u64 kernel_stack_id;
+    u8 comm[16];
+};
 
 static bool is_TCP(void *data_begin, void *data_end) {
     struct ethhdr *eth = data_begin;
@@ -31,9 +41,14 @@ SEC("tc")
 int tc_ingress(struct __sk_buff *skb) {
     void *data_end = (void *) (__u64) skb->data_end;
     void *data = (void *) (__u64) skb->data;
+    struct ethhdr *eth = data;
+    char h_dest_mac[16];
+    bpf_probe_read_kernel(&h_dest_mac, sizeof(h_dest_mac), &eth->h_dest);
 
-    if (is_TCP(data, data_end)) {
-        bpf_printk("Got IP packet");
-    }
+    char h_src_mac[16];
+    bpf_probe_read_kernel(&h_src_mac, sizeof(h_dest_mac), &eth->h_source);
+
+    
+
     return TC_ACT_OK;
 }
