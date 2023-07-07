71bbe39eaee1f72219b9994a4b9a215a07e3d905
jelipo <me@jelipo.com>
2023-07-07 18:02:54 +0800

    update

--- protocol/src/main.rs
+++ protocol/src/main.rs
@@ -1,82 +1,20 @@
 #![feature(slice_group_by)]
 
-use std::thread;
-use std::time::Duration;
+use anyhow::Result;
 
-use anyhow::{anyhow, Result};
-use byteorder::{ByteOrder, NativeEndian};
-use clap::Parser;
-use libbpf_rs::MapFlags;
-
-use common::stack::kallsyms::KallsymsCache;
-use common::stack::procsyms::ProcsymsCache;
-use crate::protocol::protocol_bss_types::key_t;
 use crate::protocol::ProtocolSkelBuilder;
-
 
 mod protocol {
     include!("./protocol.bpf.rs");
 }
 
-#[derive(Debug, Copy, Clone, Parser)]
-#[clap(name = "offcputime", about = "trace pid offcputime")]
-struct Command {
-    /// trace pid
-    #[clap(short = 'p', long)]
-    pid: u32,
-    /// wait timeout (second)
-    #[clap(short = 'f')]
-    time: u64,
-}
-
 fn main() -> Result<()> {
-    let cmd = Command::parse();
     // 初始化
     common::bump_memlock_rlimit()?;
     let builder = ProtocolSkelBuilder::default();
     let mut open_skel = builder.open()?;
-    open_skel.rodata().listen_tgid = cmd.pid;
+    open_skel.progs().sys_enter_connect().set_attach_target()
     let mut skel = open_skel.load()?;
     skel.attach()?;
-    // 等待完成
-    thread::sleep(Duration::from_secs(cmd.time));
-    // 开始处理
-    let maps = skel.maps();
-    let key_iter = maps.pid_stack_counter().keys();
-    let kallsyms_cache = KallsymsCache::new()?;
-    let procsyms_cache = ProcsymsCache::new(cmd.pid)?;
-    for key_data in key_iter {
-        let value_data = maps.pid_stack_counter().lookup(&key_data, MapFlags::ANY)?.expect("key not found");
-        let key = unsafe { std::ptr::read_unaligned(key_data.as_ptr() as *const key_t) };
-        let value = unsafe { std::ptr::read_unaligned(value_data.as_ptr() as *const u64) };
-        let comm_name = String::from_utf8_lossy(&key.comm);
-        print!("{}", comm_name.trim_end_matches(char::from(0)));
-        print_user_stacks_by_id(&maps, key.user_stack_id as u32, &procsyms_cache)?;
-        print!(";-");
-        print_kernel_stacks_by_id(&maps, key.kernel_stack_id as u32, &kallsyms_cache)?;
-        println!(" {}", value);
-    }
-    Ok(())
-}
-
-fn print_kernel_stacks_by_id(maps: &OffcputimeMaps, kernel_stack_id: u32, cache: &KallsymsCache) -> Result<()> {
-    let symbols_data = maps.stack_traces().lookup(&kernel_stack_id.to_ne_bytes(), MapFlags::ANY)?.ok_or(anyhow!("not found"))?;
-    let symbols = symbols_data.chunks(8).map(NativeEndian::read_u64).collect::<Vec<_>>();
-    for symbol in symbols.iter().rev() {
-        if *symbol != 0 {
-            print!(";{}", cache.search(*symbol));
-        }
-    }
-    Ok(())
-}
-
-fn print_user_stacks_by_id(maps: &OffcputimeMaps, stack_id: u32, cache: &ProcsymsCache) -> Result<()> {
-    let symbols_data = maps.stack_traces().lookup(&stack_id.to_ne_bytes(), MapFlags::ANY)?.ok_or(anyhow!("not found"))?;
-    let symbols = symbols_data.chunks(8).map(NativeEndian::read_u64).collect::<Vec<_>>();
-    for symbol in symbols.iter().rev() {
-        if *symbol != 0 {
-            print!(";{}", cache.search(*symbol)?);
-        }
-    }
     Ok(())
 }
--- protocol/src/protocol.bpf.c
+++ protocol/src/protocol.bpf.c
@@ -7,8 +7,12 @@ char __license[] SEC("license") = "Dual MIT/GPL";
 
 
 SEC("tracepoint/syscalls/sys_enter_connect")
+int sys_enter_connect() {
+
+    return 0;
+}
 
 SEC("tracepoint/syscalls/sys_exit_connect")
 int sys_exit_connect() {
-
+    return 0;
 }
