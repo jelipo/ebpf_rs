3c47a7d69d5f94575820bd35eb62b1f3b34eb4bb
jelipo <me@jelipo.com>
2023-07-09 01:40:14 +0800

    update

--- include/def.h
+++ include/def.h
@@ -7,3 +7,7 @@
 #define memmove(dest, src, n) __builtin_memmove((dest), (src), (n))
 #endif
 
+// 快速获取指针类型的参数
+#ifndef BPF_PTR_ARG
+#define BPF_PTR_ARG(ctx, index) ((void *) (ctx)->args[index])
+#endif
--- protocol/src/protocol.bpf.c
+++ protocol/src/protocol.bpf.c
@@ -2,39 +2,41 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 #include <bpf/bpf_core_read.h>
+#include <bpf/bpf_endian.h>
 #include "def.h"
 #include "socket_def.h"
 
 char __license[] SEC("license") = "Dual MIT/GPL";
 
+inline void print_addr(struct sockaddr *address) {
+    sa_family_t sa_family = BPF_CORE_READ_USER(address, sa_family);
+    if (sa_family != PF_INET) {
+        return;
+    }
+    struct sockaddr_in *ipv4_addr = (void *) address;
+    __be16 be_port = BPF_CORE_READ_USER(ipv4_addr, sin_port);
+    u16 port = bpf_ntohs(be_port);
+    struct in_addr be_addr = BPF_CORE_READ_USER(ipv4_addr, sin_addr);
+    u32 ip = bpf_ntohl(be_addr.s_addr);
+    bpf_printk("%d.%d.%d.%d:%d", ip >> 24 & 0xFF, ip >> 16 & 0xFF, ip >> 8 & 0xFF, ip & 0xFF, port);
+}
+
 //SEC("tracepoint/syscalls/sys_enter_connect")
 //int sys_enter_connect(struct trace_event_raw_sys_enter *ctx) {
-//    struct sockaddr *address;
-//    address = (struct sockaddr *) ctx->args[1];
-//    sa_family_t fam;
-//    bpf_core_read_user(&fam, sizeof(fam), &address->sa_family);
-//
-//    sa_family_t fam2 = BPF_CORE_READ_USER(address, sa_family);
-//    bpf_printk("%d %d ", fam, fam2);
-//
+//    struct sockaddr *address = BPF_PTR_ARG(ctx, 1);
+//    print_addr(address);
 //    return 0;
 //}
 
 
 
-//SEC("tracepoint/syscalls/sys_enter_accept")
-//int BPF_PROG(sys_enter_accept, int sockfd, struct sockaddr *_Nullable restrict addr,
-//             unsigned int *_Nullable restrict addrlen) {
-//    sa_family_t sa_family = BPF_CORE_READ(addr, sa_family);
-//    if (sa_family == AF_INET) {
-//        char sa_data[14];
-//        bpf_core_read(&sa_data, sizeof(sa_data), &addr->sa_data);
-//        bpf_printk("%d.%d.%d.%d", sa_data[0], sa_data[1], sa_data[2], sa_data[3]);
-//    } else {
-//        bpf_printk(":sys_enter_acceptfamily:%d", sa_family);
-//    }
-//    return 0;
-//}
+SEC("tracepoint/syscalls/sys_enter_accept")
+int sys_enter_accept(struct trace_event_raw_sys_enter *ctx) {
+    bpf_printk("hello");
+//    struct sockaddr *address = BPF_PTR_ARG(ctx, 1);
+//    print_addr(address);
+    return 0;
+}
 
 
 //SEC("tracepoint/syscalls/sys_enter_connect")
